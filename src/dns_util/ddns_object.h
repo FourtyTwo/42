8583a8e68563543614a0a7b0417ad5051cc3217e
sys96 <33090942+System96@users.noreply.github.com>
2018-12-15 16:01:39 -0500

    Create ddns_object.h

---
+++ src/dns_util/ddns_object.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+
+/*
+
+ddns_object.h - Data object for DDNS names
+
+*/
+
+class ddns_object {
+  public:
+  // methods
+  bool verify_ddns(); // verfies DNS object
+  std::string return_ddns_name(); // Returns DDNS name i.e mysite.42
+  std::string return_ddns_host(); // Returns DDNS IP address
+  std::string return_ddns_hash(); // Returns hash associated with DDNS
+  
+  private:
+  // variables
+  std::string ddns_name;
+  std::string ddns_host;
+  std::string ddns_hash;
+  int ddns_height; // Height when DDNS name was bought
+  };
