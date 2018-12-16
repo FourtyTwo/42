2a9a1fd8747b5e7139fa655e6070e162892c6af0
sys96 <33090942+System96@users.noreply.github.com>
2018-12-16 10:42:06 -0500

    Update ddns_object.h

--- src/dns_util/ddns_object.h
+++ src/dns_util/ddns_object.h
@@ -12,7 +12,7 @@ ddns_object.h - Data object for DDNS names
 class ddns_object {
   public:
   // methods
-  bool push_dns_info(std::string nnonce, std::string ip_host, std::string ddns_name) // very basic way of making a DDNS prefix
+  bool push_dns_info(std::string nnonce, std::string ip_host, std::string ddns_name); // very basic way of making a DDNS prefix
   bool verify_ddns(); // verfies DNS object
   std::string return_ddns_name(); // Returns DDNS name i.e mysite.42
   std::string return_ddns_host(); // Returns DDNS IP address
