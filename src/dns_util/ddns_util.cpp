b60a10e54ac116446402b1081a13dfb4a3347258
sys96 <33090942+System96@users.noreply.github.com>
2018-12-17 17:21:36 -0500

    Update ddns_util.cpp

--- src/dns_util/ddns_util.cpp
+++ src/dns_util/ddns_util.cpp
@@ -6,6 +6,12 @@
 
 #include "dns_util/ddns_util.h" // Header file for ddns_util definitions
 
+
+bool ddns_util::load_dns_from_db()
+{
+ // TODO - IMPLEMENT WHEN ddns_db IS FINISHED   
+}
+
 int ddns_util::return_dns_count()
 {
     int size = ddns_list.size(); // Returns the size of the ddns vector
@@ -16,4 +22,56 @@ std::vector<ddns_object> ddns_util::return_all_known_dns()
 {
    std::vector<ddns_object> current_objects = ddns_list;
    return current_objects; // change this later
+}
+
+ddns_object ddns_util::return_dns_info_by_hash(std::string hash)
+{
+    // find the hash in the list of all DNS names
+    for(int i = 0; i < ddns_list.size(); i++)
+    {
+        std::string prehash = ddns_list.at(i).owner_hash;
+        if(prehash == hash) // check if it matches
+        {
+            return ddns_list.at(i);   
+        }
+    }
+}
+
+ddns_object ddns_util::return_dns_info_by_name(std::string ddns_name)
+{
+    // find the name in the list of dns stuff
+    for(int i = 0; i < ddns_list.size(); i++)
+    {
+        std::string prename = ddns_list.at(i).ddns_name;
+        if(prename == ddns_name)
+        {
+            return ddns_list.at(i);
+        }
+    }
+}
+        
+std::string ddns_util::return_dns_ip_by_name(std::string ddns_name)
+{
+    // find ip by name in dns vector
+    for(int i = 0; i < ddns_list.size(); i++)
+    {
+        std::string prename = ddns_list.at(i).ddns_name;
+        if(prename == name)
+        {
+            return ddns_list.at(i).ddns_host;
+        }
+    }
+}
+
+std::string ddns_util::return_dns_ip_by_hash(std::string hash)
+{
+    // you sheep really think we landed on the moon? you believe in the moon? sheep the lot of you!
+    for(int i = 0; i < ddns_list.size(); i++)
+    {
+        std::string prehash = ddns_list.at(i).owner_hash;
+        if(owner_hash == hash)
+        {
+           return ddns_list.at(i).ddns_host;    
+        }
+    }
 }
