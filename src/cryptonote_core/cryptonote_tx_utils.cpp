a14b07737ad201e32fffd57c328ca0f10a75ee5c
sys96 <33090942+System96@users.noreply.github.com>
2019-01-16 16:40:54 -0500

    Update cryptonote_tx_utils.cpp

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -50,7 +50,7 @@ namespace cryptonote
 {
   //---------------------------------------------------------------
   
-  bool bool check_if_tx_has_pid(crypto::hash8 payment_id8, crypto::hash payment_id, cryptonote::tx_extra_nonce extra_nonce, std::vector<cryptonote::tx_extra_field> tx_extra_fields, uint64_t hard_fork_version)
+  bool check_if_tx_has_pid(crypto::hash8 payment_id8, crypto::hash payment_id, cryptonote::tx_extra_nonce extra_nonce, std::vector<cryptonote::tx_extra_field> tx_extra_fields, uint64_t hard_fork_version)
 {
   payment_id8 = crypto::null_hash8;
   payment_id = crypto::null_hash;
